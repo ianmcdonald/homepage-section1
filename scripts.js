8220527633c010c9b1a89c1c9ffcc22fe1962699
ianm <iangregoryjames@gmail.com>
2021-11-24 14:09:16 -0500

    set up nav and responsive layout

--- scripts.js
+++ scripts.js
@@ -2,3 +2,12 @@
 
 console.log('Welcome to my personal homepage!')
 
+/* OFFSCREEN MENU */
+function nav(tgl = 'toggle', cls = 'nav-active') {
+    if (document.querySelector(`.${tgl}`)) {
+        document.querySelector(`.${tgl}`).addEventListener('click', 
+            () => { document.body.classList.toggle(cls) }
+        )
+    }
+} 
+nav()
